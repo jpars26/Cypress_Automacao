5229726e9f588ac1cf8f1d48c6143366acc34cb8
jpars26 <jpars131@gmail.com>
2024-12-20 14:21:10 -0300

    Protegendo_dados

--- cypress/e2e/api_perfil.cy.js
+++ cypress/e2e/api_perfil.cy.js
@@ -1,11 +1,10 @@
 describe('Teste Api AdaPet Perfil', () =>{
-    const authorization = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIyNjQxNWMxNC1iNTYzLTQzNjItODVmMS1iZGMzOTgwNGYwNDUiLCJhZG9wdGVyTmFtZSI6IkpvYW8iLCJpYXQiOjE3MzQ2NjE0MjYsImV4cCI6MTczNDkyMDYyNn0.pufMbm9MmEiG_SP2L-cNZw4Uzf9g8yGWmaJOiU78Eqo`;
-
+    
     it('Nome na Api', () => {
         cy.request({
             method: 'GET',
             url: 'https://adopet-api-i8qu.onrender.com/adotante/perfil/26415c14-b563-4362-85f1-bdc39804f045',
-            headers: { authorization }
+            headers: Cypress.env()
         }).then((res) =>{
             cy.log(JSON.stringify(res.body));
             expect(res.status).to.be.equals(200); // Valida o status HTTP
